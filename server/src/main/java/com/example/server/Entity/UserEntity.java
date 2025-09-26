188c65b6c941c19f98e9993fe3b7f515ee3d2a01
Rohit731-prg <rohitsingha821380@gmail.com>
2025-09-26 16:35:59 +0530

    server fixed

---
+++ server/src/main/java/com/example/server/Entity/UserEntity.java
@@ -0,0 +1,24 @@
+package com.example.server.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+    
+}
